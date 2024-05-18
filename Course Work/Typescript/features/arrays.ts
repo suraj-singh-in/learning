5995e5610d8140e895346b4d494473009cfa21bc
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-18 18:26:08 +0530

    Typescript: annotations: array

---
+++ Course Work/Typescript/features/arrays.ts
@@ -0,0 +1,7 @@
+const carMakers: string[] = ["ford", "toyota", "chevy"];
+
+const carsByMacker: string[][] = [["ford"], ["toyota"], ["chevy"]];
+
+const importantDates: (Date | string)[] = [];
+importantDates.push("2023-05-04");
+importantDates.push(new Date());
