7a86e92cf8062734f5fd0d3561e5ba5ec8b27ef4
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-26 08:38:31 +0530

    NestJS: Project 2 : added ValidationPipe

--- Course Work/NestJS/PROJECT 2 - Messages/src/messages/messages.controller.ts
+++ Course Work/NestJS/PROJECT 2 - Messages/src/messages/messages.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+
   @Get()
   listMessages() {
     return [];
   }
-
-  @Get('/:id')
-  getMessage() {}
-
-  @Post()
-  createMessage() {}
 }
