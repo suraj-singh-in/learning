7952c8e8c3cf4d244e69e43a036cbefa6d96d22a
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-26 08:26:38 +0530

    NestJS : PROJECT 2 : Created a simple nestjs application using CLI

---
+++ Course Work/NestJS/PROJECT 2 - Messages/src/messages/messages.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return [];
+  }
+
+  @Get('/:id')
+  getMessage() {}
+
+  @Post()
+  createMessage() {}
+}
