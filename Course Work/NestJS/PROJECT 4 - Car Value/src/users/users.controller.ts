ea5ac2e2824666c14b98b2cfa0698b0fe755dfbe
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-27 22:13:32 +0530

    NestJS: Project 4: Update, Remove, find in user service

--- Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.controller.ts
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.controller.ts
@@ -1,8 +1,18 @@
 // Libraries
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 // DTOs
 import { createUserDto } from './dtos/create-user.dto';
+import { updatedUserDto } from './dtos/update-user.dto';
 
 // Services
 import { UsersService } from './users.service';
@@ -13,6 +23,26 @@ export class UsersController {
 
   @Post('/signup')
   createUser(@Body() body: createUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: updatedUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
