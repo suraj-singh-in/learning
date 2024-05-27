18c88d959cb7545e671873f6e178d893fd161654
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-27 21:45:15 +0530

    NestJS: Project 4: Creating and saving a user

--- Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.controller.ts
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.controller.ts
@@ -4,10 +4,15 @@ import { Body, Controller, Post } from '@nestjs/common';
 // DTOs
 import { createUserDto } from './dtos/create-user.dto';
 
+// Services
+import { UsersService } from './users.service';
+
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: createUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
--- Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.service.ts
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.service.ts
@@ -1,5 +1,18 @@
 // Libraries
 import { Injectable } from '@nestjs/common';
 
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Entities
+import { User } from './user.entity';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}
