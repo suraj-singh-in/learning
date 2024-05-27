05a8bee0affa8b01ad872b47d6430a1e7bfd4316
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-05-27 21:50:52 +0530

    NestJS: Project 4: Hooks in TypeORM

--- Course Work/NestJS/PROJECT 4 - Car Value/src/users/user.entity.ts
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/user.entity.ts
@@ -1,5 +1,12 @@
 // Libraries
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -11,4 +18,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Insert user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove user with id', this.id);
+  }
 }
