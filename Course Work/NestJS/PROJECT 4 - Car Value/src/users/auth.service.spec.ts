85393b20b7217e868f9eb38253e6d2523a0994f5
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-06-08 08:30:52 +0530

    NestJS: Project 4: Test cases

---
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/auth.service.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { BadRequestException } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    // Create a fake copy of users service
+    fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
+    // Create a new DI Container
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('suraj@gmail.com', 'asdf');
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(user.password).not.toEqual('asdf');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+});
