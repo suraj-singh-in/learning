3461a81b8cd5af59b6130930dd1217dbb01ef93d
suraj-singh-in <suraj.singh.in.delhi@gmail.com>
2024-06-08 21:10:56 +0530

    NestJS: Project 4: User Service Test fix

--- Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.service.spec.ts
+++ Course Work/NestJS/PROJECT 4 - Car Value/src/users/users.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let fakeUserRepo: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: fakeUserRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
